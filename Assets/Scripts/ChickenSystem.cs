31517600a917a3e494d396f36176d30fefd9f38f
Lee Dongyun <notonalcyone@gmail.com>
2016-11-24 03:30:07 +0900

    Basic System
    build basic system of application

---
+++ Assets/Scripts/ChickenSystem.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChickenSystem : MonoBehaviour {
+
+    public UnityEngine.UI.Text ChickenStackDisplay;
+    public UnityEngine.UI.Text ChickenStatDisplay;
+    public int ChickenStack;
+    public int IncreaseChickenStack;
+    public int MaxChickenStack;
+    public int MinChickenStack;
+    public int CurrentChickenStat;
+    public int CurrentChickenIncrease;
+
+    // 시작시 호출
+    void Start () {
+        CurrentChickenStat = 0;
+        ChickenStack = 0;
+        MinChickenStack = 0;
+        //이 아래는 임시
+        MaxChickenStack = 100;
+        IncreaseChickenStack = 10;
+        CurrentChickenIncrease = 5;
+
+        InvokeRepeating("ChickenIncrease",1.0f,1.0f); //시작후 1초후부터 매 1 초마다 ChickenIncrease 호출
+	}
+	
+    void ChickenIncrease ()
+    {
+      if (ChickenStack < MaxChickenStack)
+        {
+            ChickenStack += IncreaseChickenStack; //스택된 치킨이 최대치보다 적을경우 스택에 증가량을 더함
+            
+        }
+        
+    }
+
+    // 프레임마다 호출
+    void Update ()
+    {
+        ChickenStackDisplay.text = "나뒹구는치킨: " + ChickenStack;
+        ChickenStatDisplay.text = "주운 치킨: " + CurrentChickenStat;
+        ChickenStack = Mathf.Clamp(ChickenStack, MinChickenStack, MaxChickenStack);
+    }
+    public void ChickenMineClicked()
+    {
+        if (ChickenStack > MinChickenStack)
+        {
+            CurrentChickenStat += CurrentChickenIncrease;
+            ChickenStack -= CurrentChickenIncrease;
+        }
+    }
+}
---
+++ Assets/Scripts/UpgradeSystem.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpgradeSystem : MonoBehaviour {
+
+
+    public ChickenSystem Chicken;
+    public UnityEngine.UI.Text LevelDisplay;
+    public int UpgradeLevel;
+    public int UpgradePrice;
+
+
+    void Start()
+    {
+        UpgradeLevel = 1; // default upgarde level is 1
+        UpgradePrice = 1;
+
+    }
+    void Update()
+    {
+        LevelDisplay.text = "Current_level: " + UpgradeLevel;
+    }
+    public void UpgradeClick()
+    {
+        if (Chicken.CurrentChickenStat >= UpgradePrice)
+        {
+            Chicken.CurrentChickenStat -= UpgradePrice;
+            UpgradeLevel++;
+        }
+
+    }
+
+}
